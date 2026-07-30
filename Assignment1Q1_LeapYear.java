1372f53358a3e7bf79b3bc7a504fc2c188802d06
akshtprakash9106 <akshtprakash@gmail.com>
2026-07-30 10:51:47 +0530

    Update Assignment1Q1_LeapYear.java

--- Assignment1Q1_LeapYear.java
+++ Assignment1Q1_LeapYear.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
+
+public class LeapYearCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a year: ");
+        int year = sc.nextInt();
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+
+        sc.close();
+    }
+}
 
